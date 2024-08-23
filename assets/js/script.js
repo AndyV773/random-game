8c39f69bce9410531da60b92b2ebb78cfbce9894
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-08-23 20:25:37 +0000

    add win lose function and add funds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,42 @@
 // some code taken from love maths project
-// wait for the dom to finish loading before running the game
-// get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "play-button") {
-                runGame();
-            } else if (this.getAttribute("data-type") === "add-player") {
-                addPlayer();
-            } else if (this.getAttribute("data-type") === "rmv-player") {
-                rmvPlayer();
-            } else if (this.getAttribute("data-type") === "change-stake") {
-                alert('stake changed');
-            } else if (this.getAttribute("data-type") === "add-funds") {
-                alert('funds added');
-            } else if (this.getAttribute("data-type") === "cash-out") {
-                alert('you took the money');
-            } else {
-                alert("Function Unknown");
-            }
-        });
-    }
-})
+// list of global variables 
+const rmvButton = document.getElementById("rmv-player");
+const addButton = document.getElementById("add-player");
+const playButton = document.getElementById("play-button");
+const stakeButton = document.getElementById("change-stake");
+const addFundsButton = document.getElementById("add-funds");
 
 /**
  * gets two random numbers one for the computer and one for the user
  * pairs them up with a card index 
  * adds the numbers to the corresponding cards
  */
-function runGame() {
+function runGame(event) {
+
     let cardArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"];
 
     // random array from https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array
     let num1 = cardArray[Math.floor(Math.random() * cardArray.length)];
     let num2 = cardArray[Math.floor(Math.random() * cardArray.length)];
     let num3 = cardArray[Math.floor(Math.random() * cardArray.length)];
-    let computerCard = document.getElementById("computers-card");
-    let user1Card = document.getElementById("user1-card");
 
-    computerCard.innerText = num1;
-    user1Card.innerText = num2;
 
-    let user2 = document.getElementById("user2-card");
-    if (user2) {
-        user2.innerHTML = num3;
-    };
+    if (event) {
+        let computerCard = document.getElementById("computers-card");
+        let user1Card = document.getElementById("user1-card");
+
+        computerCard.innerText = num1;
+        user1Card.innerText = num2;
+
+        let user2 = document.getElementById("user2-card");
+        if (user2) {
+            user2.innerHTML = num3;
+        };
+    }
+
+    checkResultPlayer1(num1, num2);
+    checkResultPlayer2(num1, num3);
 
 }
 
@@ -55,7 +44,36 @@ function runGame() {
  * gets the card numbers from runGame() and works out the higher card
  * then returns the winner 
  */
-function checkResult() {
+function checkResultPlayer1(num1, num2) {
+
+    if (runGame)
+        if (num1 < num2) {
+            ifPlayer1Wins();
+            console.log("Player 1 Wins!");
+        } else if (num1 === num2) {
+        console.log("Player 1 Draw!");
+    } else {
+        ifComputerWins();
+        console.log("Player 1 Loses!");
+    };
+
+}
+
+function checkResultPlayer2(num1, num3) {
+
+    let user2 = document.getElementById("user2-card");
+    if (user2) {
+        if (runGame)
+            if (num1 < num3) {
+                ifPlayer2Wins();
+                console.log("Player 2 Wins!");
+            } else if (num1 === num3) {
+            console.log("Player 2 Draw!");
+        } else {
+            ifComputerWins();
+            console.log("Player 2 Loses!");
+        };
+    };
 
 }
 
@@ -103,3 +121,72 @@ function rmvPlayer() {
         alert("You can't remove anymore players");
     };
 }
+
+// function stakeValue() {
+//     let stakeBox = document.getElementsByClassName("stake-box");
+
+//     let stake5 = document.getElementById("stake-5");
+//     let stake10 = document.getElementById("stake-10");
+//     let stake20 = document.getElementById("stake-20");
+
+//     // stake5.style.backgroundColor = "white";
+//     // stake10.style.backgroundColor = "white";
+//     // stake20.style.backgroundColor = "white";
+
+//     if (this.style.backgroundColor === "white") {
+//         stakeBox[0].style.backgroundColor = "orange";
+//     } else if (stake5.style.backgroundColor === "orange" && stake10.style.backgroundColor === "white" && stake20.style.backgroundColor === "white") {
+//         stake10.style.backgroundColor = "orange";
+//     } else if (stake5.style.backgroundColor === "white" && stake10.style.backgroundColor === "orange" && stake20.style.backgroundColor === "white") {
+//         stake20.style.backgroundColor = "orange";
+//     } else {
+//         alert('error')
+//     };
+
+// }
+
+function addFunds() {
+    let oldFunds = parseInt(document.getElementById("player-funds").innerText);
+    let funds = document.getElementById("player-funds").innerText = oldFunds + 5;
+    let max = 1000;
+
+    if (funds > max) {
+        alert("Sorry you can't add anymore funds");
+    }
+
+}
+
+function ifComputerWins() {
+
+    let oldComputerScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = oldComputerScore + 5;
+
+}
+
+function ifPlayer1Wins() {
+
+    let oldPlayer1Score = parseInt(document.getElementById("player1-score").innerText);
+    document.getElementById("player1-score").innerText = oldPlayer1Score + 5;
+
+}
+
+function ifPlayer2Wins() {
+
+    let user2 = document.getElementById("user2-card");
+    if (user2) {
+        let oldPlayer2Score = parseInt(document.getElementById("player2-score").innerText);
+        document.getElementById("player2-score").innerText = oldPlayer2Score + 5;
+    };
+
+}
+
+// get the button elements and add event listeners to them
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    rmvButton.addEventListener("click", rmvPlayer);
+    addButton.addEventListener("click", addPlayer);
+    playButton.addEventListener("click", runGame);
+    // stakeButton.addEventListener("click", stakeValue);
+    addFundsButton.addEventListener("click", addFunds);
+
+});
